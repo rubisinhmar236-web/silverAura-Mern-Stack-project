4e661f71dc8fbe53f6c069aea20ea3bff6a645fa
rubisinhmar <rubisinhmar236@gmail.com>
2026-08-18 11:13:33 +0530

    use bootstrap, add navbar(icons and sections), add some css styling

--- app.js
+++ app.js
@@ -12,6 +12,9 @@ const listings = require("./listing");
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.engine("ejs", ejsMate);
+app.use(express.static(path.join(__dirname, "/public")));
+
+
 main()
 .catch(err => console.log(err));
 
